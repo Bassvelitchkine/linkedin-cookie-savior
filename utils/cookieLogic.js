38a05a3b00cfd9641660b82618ec1d98701dd1b8
Bastiche <bastien.velitchkine@live.fr>
2022-06-22 13:38:55 +0200

    ⚡️ Async cookie update and cookie update debug

--- utils/cookieLogic.js
+++ utils/cookieLogic.js
@@ -1,32 +1,57 @@
 import { isKeyInObject } from "./utils.js";
 
+export const retrieveAndUpdateCookie = async (platformName, cookieDetails) => {
+    return new Promise((resolve, _) => {
+        chrome.cookies.get(cookieDetails, cookie => {
+            createOrUpdateCookie(platformName, cookieDetails["name"], cookie["value"], cookie["expirationDate"]);
+            resolve(cookie);
+        });
+    });
+};
+
 // Function to check the storage and look for the cookie of a specific platform
 // Updates the cookie value if needed
-export const createOrUpdateCookie = (platformName, cookieName, cookieValue, expirationDate) => {
+const createOrUpdateCookie = (platformName, cookieName, cookieValue, expirationDate) => {
     let storedAt = new Date(Date.now());
     storedAt = storedAt.toISOString();
 
-    const cookieObject = {
+    const cookieParams = {
         "value": cookieValue,
         "expires_at": expirationDate,
         "stored_at": storedAt
-    }
+    };
 
-    chrome.storage.local.get(platformName, ({platform}) => {
-        if (isKeyInObject(platform, cookieName)){
-            const oldCookieValue = platform[cookieName];
-            if (oldCookieValue !== cookieValue){
-                platform[cookieName] = cookieObject;
-                chrome.storage.local.set(platform, _ => console.log(`Updated ${cookieName}`));
+    chrome.storage.local.get(platformName, platformStorage => {
+        if (platformStorage && platformStorage[platformName]){
+            if (isKeyInObject(platformStorage[platformName], cookieName)){
+                const oldCookieValue = platformStorage[platformName][cookieName]["value"];
+                if (oldCookieValue !== cookieValue){
+                    platformStorage[platformName][cookieName] = cookieParams;
+                    chrome.storage.local.set(platformStorage, _ => console.log(`Updated ${cookieName}`));
+                    if (platformName == "linkedin"){
+                        console.log("Will send a request");
+                    }
+                }
+            } else {
+                platformStorage[platformName][cookieName] = cookieParams;
+                chrome.storage.local.set(platformStorage, _ => console.log(`Created ${cookieName}`));
                 if (platformName == "linkedin"){
                     console.log("Will send a request");
                 }
             }
         } else {
-            platform[cookieName] = cookieObject;
-            chrome.storage.local.set(platform, _ => console.log(`Created ${cookieName}`));
+            let cookieObject = {};
+            cookieObject[cookieName] = cookieParams;
+
+            const platformStorage = {};
+            platformStorage[platformName] = cookieObject;
+
+            chrome.storage.local.set(platformStorage, _ => console.log(`Created ${cookieName}`));
+            if (platformName == "linkedin"){
+                console.log("Will send a request");
+            }
         }
-    })
+    });
 };
 
 // Function to send a request to the webhook according to the apiRequest schema
