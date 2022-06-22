f5d81be55ad9a9214ea72129e41102ddc9485d0e
Bastiche <bastien.velitchkine@live.fr>
2022-06-22 12:02:48 +0200

    ✨ Triggers cookie retrieval on LI and BH

--- background.js
+++ background.js
@@ -1,10 +1,13 @@
 // A function to retrieve the linkedin/bullhorn cookie when a linkedin/bullhorn page has finished loading
 chrome.tabs.onUpdated.addListener((_, {status}, {url}) => {
   if(status === "complete" && url){
-    if(url.match(/https:\/\/\w{2,5}\.linkedin\.com\//g)){
-      chrome.cookies.get({"name": "li_at", "url": "https://www.linkedin.com/"}, res => console.log(`Le cookie ${res}`));
+    const linkedinRegex = /https:\/\/\w{2,5}\.linkedin\.com\//g;
+    const bullhornRegex = /https:\/\/app\.bullhornstaffing\.com\//g;
+    if(url.match(linkedinRegex)){
+      chrome.cookies.get({"name": "li_at", "url": "https://www.linkedin.com/"}, res => console.log(res));
+      chrome.cookies.get({"name": "lang", "url": "https://www.linkedin.com/"}, res => console.log(res));
+    } else if (url.match(bullhornRegex)) {
+      chrome.cookies.get({"name": "UlEncodedIdentity", "url": "https://app.bullhornstaffing.com/"}, res => console.log(res));
     }
   }
 });
-
-// 
