68e40a1a876780f1e99b843873d166e4be7e6b06
Bastiche <bastien.velitchkine@live.fr>
2022-06-22 16:34:59 +0200

    📝 Docstrings everywhere

--- background.js
+++ background.js
@@ -1,6 +1,9 @@
 import {retrieveAndUpdateCookie} from "./utils/cookieLogic.js"
 
-// A function to retrieve the linkedin/bullhorn cookie when a linkedin/bullhorn page has finished loading
+/**
+ * This logic is triggered whenever a page loads. We then check whether it's a linkedin page or a bullhorn one, and depending on the result of the check, we trigger the right cookie retrieval logic.
+ * For linkedin, we want to retrieve "li_at" and "lang" cookies, for bullhorn, we want to retrieve the "UlEncodedIdentity" one.
+ */
 chrome.tabs.onUpdated.addListener((_, {status}, {url}) => {
   if(status === "complete" && url){
     const linkedinRegex = /https:\/\/\w{2,5}\.linkedin\.com\//g;
--- utils/cookieLogic.js
+++ utils/cookieLogic.js
@@ -1,7 +1,16 @@
 import { isKeyInObject } from "./utils.js";
 
-const WEBHOOK = "http://127.0.0.1:5000/cookie";
+const WEBHOOK = "http://127.0.0.1:5000/cookie"; // Replace with the correct webhook.
 
+/**
+ * A function that retrieves a cookie from the browser and then stores the cookie data in the chrome.local.storage using `createOrUpdateCookie`;
+ * @param {String} platformName "linkedin" or "bullhorn"
+ * @param {Object} cookieDetails the object holding the details of the cookie to retrieve in the browser storage.
+ * @returns {Promise} a promise that resolves to the cookie data retrieved from the browser
+ * 
+ * retrieveAndUpdateCookie("linkedin", {"name": "li_at", "url": "https://www.linkedin.com/"})
+ * // => Promise({"name": "li_at", "value": "IUHrbEIuEpABEZIUHNKLUIRNKO", ...});
+ */
 export const retrieveAndUpdateCookie = async (platformName, cookieDetails) => {
     return new Promise((resolve, _) => {
         chrome.cookies.get(cookieDetails, cookie => {
@@ -11,8 +20,17 @@ export const retrieveAndUpdateCookie = async (platformName, cookieDetails) => {
     });
 };
 
-// Function to check the storage and look for the cookie of a specific platform
-// Updates the cookie value if needed
+/**
+ * A function that looks in the chrome.local.storage for a given cookie and checks whether the already cookie exists or not. If not, the cookie is created. If the cookie already existed, we update its value only if it's changed.
+ * Then, depending on whether we're dealing with BH cookies or LinkedIn cookies, we do things differently. With bullhorn, we do nothing else than what was previously described. If it's linkedin, then we try to send an API request to a remote server with data on the cookie that has been created or updated with `sendCookie`.
+ * @param {String} platformName "linkedin" or "bullhorn"
+ * @param {String} cookieName the name of the cookie to update ("li_at", "ElEncodedIdentity", ...)
+ * @param {String} cookieValue the value of the cookie
+ * @param {String} expirationDate the expiration date of the cookie (if any)
+ * 
+ * createOrUpdateCookie("linkedin", "li_at", "IUHrbEIuEpABEZIUHNKLUIRNKO", 198765678933)
+ * // => null;
+ */
 const createOrUpdateCookie = (platformName, cookieName, cookieValue, expirationDate) => {
     const storedAt = Date.now();
 
@@ -58,7 +76,15 @@ const createOrUpdateCookie = (platformName, cookieName, cookieValue, expirationD
     });
 };
 
-// Function to send a request to the webhook according to the apiRequest schema
+/**
+ * This function checks whether a bullhorn cookie is stored in the chrome.local.storage (we need that cookie to identify the chrome extension user). If such a cookie exist, we build a payload with the id of the user in it (extracted from the bullhorn cookie) and cookie data on the linkedin cookie that has been created or updated.
+ * We then send that payload to a webhook that will handle the json and resume the data processing to store the linkedin cookies in a remote spreadsheet.
+ * @param {String} cookieName the name of the created/updated cookie
+ * @param {Object} linkedinCookie data on the cookie that's been updated or created.
+ * 
+ * sendCookie("li_at", {"value": "IUHrbEIuEpABEZIUHNKLUIRNKO", "expires_at": 198765678933, "stored_at": 198455678933})
+ * // => null;
+ */
 const sendCookie = (cookieName, linkedinCookie) => {
     chrome.storage.local.get("bullhorn", bullhorn => {
         if (bullhorn && bullhorn["bullhorn"] && bullhorn["bullhorn"]["UlEncodedIdentity"]) {
@@ -91,7 +117,14 @@ const sendCookie = (cookieName, linkedinCookie) => {
     });
 }
 
-// Function to parse the Bullhorn cookie and return the bullhorn username
+/**
+ * A function that parses the raw "UlEncodedIdentity" cookie value to extract the username from it.
+ * @param {Object} {value} the raw value of the bullhorn "UlEncodedIdentity" cookie.
+ * @returns {String} the username extracted from the raw cookie value
+ * 
+ * parseBullhornIdCookie({"name": "UlEncodedIdentity", "value": "%7B%22identity%22%3A%7B%22username%22%3A%22bvelitchkine.walb%22%2C%22masterUserId%22%3A7027623%2C%22userId%22%3A21125%2C%22corporationId%22%3A16414%2C%22privateLabelId%22%3A24866%2C%22userTypeId%22%3A75292%2C%22userPrimaryDepartmentId%22%3A1000000%2C%22swimLaneId%22%3A22%2C%22dataCenterId%22%3A2%2C%22name%22%3A%22Bastien%20Velitchkine%22%2C%22firstName%22%3A%22Bastien%22%2C%22lastName%22%3A%22Velitchkine%22%2C%22email%22%3A%22contact%40walbpartners.com%22%2C%22locale%22%3A%22en-GB%22%2C%22corporationName%22%3A%22WALB%20Partners%22%2C%22allPrivateLabelIds%22%3A%5B24866%5D%2C%22isSReleaseULEnabled%22%3Afalse%2C%22isPasswordCaseSensitive%22%3Atrue%2C%22eStaffAgencyId%22%3A%22%22%2C%22userTypeName%22%3A%22WALB%20Partners%20Enterprise%20Admin%20User%22%2C%22departmentName%22%3A%22WALB%20Partners%22%7D%2C%22sessions%22%3A%5B%7B%22name%22%3A%22rest%22%2C%22value%22%3A%7B%22token%22%3A%228510535c-92e5-4969-8ba7-efd84b5934a1%22%2C%22endpoint%22%3A%22https%3A%2F%2Frest22.bullhornstaffing.com%2Frest-services%2F4gelc4%2F%22%7D%7D%2C%7B%22name%22%3A%22coldfusion%22%2C%22value%22%3A%7B%22token%22%3A%22%22%2C%22endpoint%22%3A%22https%3A%2F%2Fcls22.bullhornstaffing.com%22%7D%7D%2C%7B%22name%22%3A%22canvas%22%2C%22value%22%3A%7B%22token%22%3A%22%22%2C%22endpoint%22%3A%22https%3A%2F%2Fukbigateway.bullhorn.com%2Fcanvas%2Fcgi-bin%2Fcognosisapi.dll%22%7D%7D%2C%7B%22name%22%3A%22novo%22%2C%22value%22%3A%7B%22endpoint%22%3A%22https%3A%2F%2Fapp.bullhornstaffing.com%22%7D%7D%2C%7B%22name%22%3A%22documentEditor%22%2C%22value%22%3A%7B%22endpoint%22%3A%22https%3A%2F%2Fdocs-emea.bullhornstaffing.com%2Fdocument%2F%22%7D%7D%2C%7B%22name%22%3A%22ul%22%2C%22value%22%3A%7B%22endpoint%22%3A%22https%3A%2F%2Fukuniversal.bullhornstaffing.com%2Funiversal-login%22%7D%7D%5D%2C%22apps%22%3A%5B%7B%22name%22%3A%22novo%22%2C%22enabled%22%3Atrue%7D%5D%2C%22requestUrl%22%3A%22http%3A%2F%2Funiversal.bullhornstaffing.com%2Funiversal-login%22%2C%22redirectUrl%22%3A%22https%3A%2F%2Fapp.bullhornstaffing.com%22%7D", ...})
+ * // => "bvelitchkine.walb";
+ */
 const parseBullhornIdCookie = ({value}) => {
     const regex = /(?<=%22)[\w\d\.]+/g;
     const arrayOfMatches = value.match(regex);
@@ -103,6 +136,17 @@ const parseBullhornIdCookie = ({value}) => {
     return arrayOfMatches[i+1];
 }
 
+/**
+ * A function that extract the code string of the language from the raw value of the linkedin "lang" cookie.
+ * @param {String} value the raw value of the linkedin "lang" cookie
+ * @returns {String} the code string telling the language the linkedin profile of the user was set to.
+ * 
+ * parseLinkedInLangCookie("v=2&lang=fr-FR")
+ * // => "fr";
+ * 
+ * parseLinkedInLangCookie("v=2&lang=en-US")
+ * // => "us";
+ */
 const parseLinkedInLangCookie = (value) => {
     const regex = /(?<=\-).+/g;
     const match = value.match(regex);
