25d7bb9f05bb89ea7341fe4b8ccdad63a2b71b14
Bastiche <bastien.velitchkine@live.fr>
2022-06-22 13:39:38 +0200

    🎨 Async cookie retrieval and logic triggering

--- background.js
+++ background.js
@@ -1,13 +1,19 @@
+import {retrieveAndUpdateCookie} from "./utils/cookieLogic.js"
+
 // A function to retrieve the linkedin/bullhorn cookie when a linkedin/bullhorn page has finished loading
 chrome.tabs.onUpdated.addListener((_, {status}, {url}) => {
   if(status === "complete" && url){
     const linkedinRegex = /https:\/\/\w{2,5}\.linkedin\.com\//g;
     const bullhornRegex = /https:\/\/app\.bullhornstaffing\.com\//g;
+
     if(url.match(linkedinRegex)){
-      chrome.cookies.get({"name": "li_at", "url": "https://www.linkedin.com/"}, res => console.log(res));
-      chrome.cookies.get({"name": "lang", "url": "https://www.linkedin.com/"}, res => console.log(res));
+      const platformName = "linkedin";
+      retrieveAndUpdateCookie(platformName, {"name": "li_at", "url": "https://www.linkedin.com/"})
+      .then(_ => {
+        return retrieveAndUpdateCookie(platformName, {"name": "lang", "url": "https://www.linkedin.com/"});
+      });
     } else if (url.match(bullhornRegex)) {
-      chrome.cookies.get({"name": "UlEncodedIdentity", "url": "https://app.bullhornstaffing.com/"}, res => console.log(res));
+      retrieveAndUpdateCookie("bullhorn", {"name": "UlEncodedIdentity", "url": "https://app.bullhornstaffing.com/"});
     }
   }
 });
