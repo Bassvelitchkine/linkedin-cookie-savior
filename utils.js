ecdb9588b9ad87bcfc7b35683616b7fdb5cfc75d
Bastiche <bastien.velitchkine@live.fr>
2022-06-22 11:59:21 +0200

    🔥 Moved utils.js to utils folder

--- utils.js
+++
@@ -1,6 +0,0 @@
-// Function to check the storage and look for the cookie of a specific platform
-// Returns the cookie value if it exists, null otherwise
-
-// Function to send a request to the webhook according to the apiRequest schema
-
-// Function to parse the Bullhorn cookie and return the bullhorn username
