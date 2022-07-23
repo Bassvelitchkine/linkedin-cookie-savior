42620b80ef90130b51979c43c073dbe8efc3e58a
Bastiche <bastien.velitchkine@live.fr>
2022-07-23 19:23:12 +0200

    ✨ publicIdentifier stored in chrome storage

--- background.js
+++ background.js
@@ -1,5 +1,6 @@
 import {retrieveAndUpdateCookie} from "./utils/cookieLogic.js"
 import {extractPublicIdentifier} from "./contentScripts/extractPublicIdentifier.js"
+import { savePublicIdentifier } from "./utils/utils.js";
 
 /**
  * This logic is triggered whenever a page loads. We then check whether it's a linkedin page or a bullhorn one, and depending on the result of the check, we trigger the right cookie retrieval logic.
@@ -16,10 +17,12 @@ chrome.tabs.onUpdated.addListener((tabId, {status}, {url}) => {
         target: {tabId},
         function: extractPublicIdentifier
       }, payload => {
-        console.log(payload[0]["result"]);
-        retrieveAndUpdateCookie(platformName, {"name": "li_at", "url": "https://www.linkedin.com/"})
+        savePublicIdentifier(payload[0]["result"])
         .then(_ => {
-          return retrieveAndUpdateCookie(platformName, {"name": "lang", "url": "https://www.linkedin.com/"});
+          retrieveAndUpdateCookie(platformName, {"name": "li_at", "url": "https://www.linkedin.com/"})
+          .then(_ => {
+            return retrieveAndUpdateCookie(platformName, {"name": "lang", "url": "https://www.linkedin.com/"});
+          });
         });
       });
     } else if (url.match(bullhornRegex)) {
--- utils/utils.js
+++ utils/utils.js
@@ -16,3 +16,57 @@
 export const isKeyInObject = (object, key) => {
     return Object.keys(object).reduce((acc, k) => acc || (k == key), false);
 }
+
+/**
+ * An ASYNCHRONOUS function to retrieve any value in the chrome storage.local unit, if it exists. We created this function just to benefit from the promises and program asynchronously.
+ * @param {String} key the key we're looking for in the chrome.storage.local unit
+ * @returns {Promise} a promise that resolves to undefined if the key did not exist in chrome.storage.local, and to the value associated to the key otherwise.
+ * 
+ * readLocalStorage("toggle")
+ * // => Promise(true);
+ */
+ const readLocalStorage = async (key) => {
+    return new Promise((resolve, _) => {
+      chrome.storage.local.get([key], function (result) {
+        if (result[key] === undefined) {
+          resolve(undefined);
+        } else {
+          resolve(result[key]);
+        }
+      });
+    });
+  };
+
+/**
+ * An ASYNCHRONOUS function to set any value in the chrome storage.local unit. We created this function just to benefit from the promises and program asynchronously. It's an asynchronous version of  chrome.storage.local.set.
+ * @param {Object} object the object to write in the chrome.storage.local unit, according to chrome.storage.local.set (they both have identic parameters).
+ * @returns {Promise} a promise that resolves to the object we just stored in the storage unit
+ * 
+ * writeToLocalStorage({"toggle": false})
+ * // => Promise({"toggle": false});
+ */
+const writeToLocalStorage = async (object) => {
+    return new Promise((resolve, _) => {
+        chrome.storage.local.set(object, _ => resolve(object));
+    });
+};
+
+export const savePublicIdentifier = async (publicIdentifier) => {
+    const time = Date.now();
+    const payload = {
+        "publicIdentifier": {
+            "value": publicIdentifier,
+            "expires_at": null,
+            "stored_at": time
+        }
+    };
+
+    return readLocalStorage("linkedin")
+    .then(object => {
+        if (object && object["linkedin"]){
+            return writeToLocalStorage({"linkedin": {...object["linkedin"], ...payload}});
+        } else {
+            return writeToLocalStorage({"linkedin": payload});
+        }
+    });
+}
